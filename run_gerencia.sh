1568e0b5a63f76b0002c96961a48673878ee777e
Joahannes <joahannes@lrc.ic.unicamp.br>
2018-09-26 16:11:11 -0300

    Adicionado parametro de mobilidade

--- run_gerencia.sh
+++ run_gerencia.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#Autor: Joahannes B.D. Costa <joahannes@gmail.com>
+#Autor: Joahannes B.D. Costa <joahannes@lrc.ic.unicamp.br>
 #Data: 23.09.2018
 #Nota: script para rodar simulacao de MO655 - Gerencia de Redes - 2018
-#Rodar: ./run_gerencia.sh numero_seeds numero_clientes trafego
+#Rodar: ./run_gerencia.sh numero_seeds numero_clientes trafego mobilidade
 
 seeds=$1
 #$2 = clientes
@@ -11,9 +11,18 @@ seeds=$1
 #Vai para diretorio ns-3.29
 cd ..
 
+#Resumo da simulacao
 echo
+echo " * MO655 - Gerência de Redes - 2018"
 echo " * Número de seeds: $1"
 echo " * Número de clientes: $2"
+if [ $4 == 0 ]
+then
+	echo " * Mobilidade: Ausente"
+else
+	echo " * Mobilidade: Presente"
+fi
+#--
 if [ $3 == 0 ]
 then
 	echo " * Tráfego: UDP"
@@ -27,34 +36,46 @@ echo
 
 for ((i=0; i<$seeds; i++))
 do
-	#Controle para mensagens
-	if [ $3 == 0 ]
+	if [ $4 == 0 ]
 	then
-		echo "Executando seed número [$i] com $2 Clientes com UDP..."
-		#Chamada da simulacao
-		./waf --run "scratch/gerencia2018 --seed=$i --nWifi=$2 --nApWifi=1 --verbose=true --tracing=false --trafego=$3" > resultados/results-udp-$i.txt 2>&1
-	elif [ $3 == 1 ]
-	then
-		echo "Executando seed número [$i] com $2 Clientes com TCP..."
-		#Chamada da simulacao
-		./waf --run "scratch/gerencia2018 --seed=$i --nWifi=$2 --nApWifi=1 --verbose=true --tracing=false --trafego=$3" > resultados/results-tcp-$i.txt 2>&1
+		#Controle para mensagens
+		if [ $3 == 0 ]
+		then
+			echo "Executando seed [$i] com $2 clientes ESTÁTICOS e tráfego UDP..."
+			#Chamada da simulacao
+			./waf --run "scratch/gerencia2018 --seed=$i --nWifi=$2 --trafego=$3 -mobilidade=$4" > resultados/results-est-udp-$i.txt 2>&1
+		elif [ $3 == 1 ]
+		then
+			echo "Executando seed [$i] com $2 clientes ESTÁTICOS e tráfego TCP..."
+			#Chamada da simulacao
+			./waf --run "scratch/gerencia2018 --seed=$i --nWifi=$2 --trafego=$3 -mobilidade=$4" > resultados/results-est-tcp-$i.txt 2>&1
+		else
+			echo "Executando seed [$i] com $2 clientes ESTÁTICOS e tráfegos UDP/TCP..."
+			#Chamada da simulacao
+			./waf --run "scratch/gerencia2018 --seed=$i --nWifi=$2 --trafego=$3 -mobilidade=$4" > resultados/results-est-ambos-$i.txt 2>&1
+		fi
 	else
-		echo "Executando seed número [$i] com $2 Clientes com UDP e TCP..."
-		#Chamada da simulacao
-		./waf --run "scratch/gerencia2018 --seed=$i --nWifi=$2 --nApWifi=1 --verbose=true --tracing=false --trafego=$3" > resultados/results-ambos-$i.txt 2>&1
+		#Controle para mensagens
+		if [ $3 == 0 ]
+		then
+			echo "Executando seed [$i] com $2 clientes MÓVEIS e tráfego UDP..."
+			#Chamada da simulacao
+			./waf --run "scratch/gerencia2018 --seed=$i --nWifi=$2 --trafego=$3 -mobilidade=$4" > resultados/results-mov-udp-$i.txt 2>&1
+		elif [ $3 == 1 ]
+		then
+			echo "Executando seed [$i] com $2 clientes MÓVEIS e tráfego TCP..."
+			#Chamada da simulacao
+			./waf --run "scratch/gerencia2018 --seed=$i --nWifi=$2 --trafego=$3 -mobilidade=$4" > resultados/results-mov-tcp-$i.txt 2>&1
+		else
+			echo "Executando seed [$i] com $2 clientes MÓVEIS e tráfegos UDP/TCP..."
+			#Chamada da simulacao
+			./waf --run "scratch/gerencia2018 --seed=$i --nWifi=$2 --trafego=$3 -mobilidade=$4" > resultados/results-mov-ambos-$i.txt 2>&1
+		fi
 	fi
 done
 
 echo
 echo "Fim da simulação! :)"
 echo
-
-#cd resultados
-#gnuplot Atraso.plt
-#gnuplot Vazao.plt
-
-#cd ../../netanim-3.108/
-#./NetAnim
-
 
 #EOF
