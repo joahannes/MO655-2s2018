f67499a7763762f4af49c307c0ed4006436b6d49
Joahannes Bruno <joahannes@lrc.ic.unicamp.br>
2018-10-14 17:46:59 -0300

    Adicionado cenário com 1 cliente

--- run_all.sh
+++ run_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #Estaticos
-./run_gerencia.sh 33 2 0 0 && ./run_gerencia.sh 33 4 0 0 && ./run_gerencia.sh 33 6 0 0 && ./run_gerencia.sh 33 2 1 0 && ./run_gerencia.sh 33 4 1 0 && ./run_gerencia.sh 33 6 1 0
+./run_gerencia.sh 33 1 0 0 && ./run_gerencia.sh 33 2 0 0 && ./run_gerencia.sh 33 4 0 0 && ./run_gerencia.sh 33 6 0 0 && ./run_gerencia.sh 33 1 1 0 && ./run_gerencia.sh 33 2 1 0 && ./run_gerencia.sh 33 4 1 0 && ./run_gerencia.sh 33 6 1 0
 
 #Moveis
-./run_gerencia.sh 33 2 0 1 && ./run_gerencia.sh 33 4 0 1 && ./run_gerencia.sh 33 6 0 1 && ./run_gerencia.sh 33 2 1 1 && ./run_gerencia.sh 33 4 1 1 && ./run_gerencia.sh 33 6 1 1
+./run_gerencia.sh 33 1 0 1 && ./run_gerencia.sh 33 2 0 1 && ./run_gerencia.sh 33 4 0 1 && ./run_gerencia.sh 33 6 0 1 && ./run_gerencia.sh 33 1 1 1 && ./run_gerencia.sh 33 2 1 1 && ./run_gerencia.sh 33 4 1 1 && ./run_gerencia.sh 33 6 1 1
 
 #email
 #python email_proc.py
