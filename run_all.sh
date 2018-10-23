9967e132f5c716e877c4aafc64b3ed291f4d541b
Joahannes Costa <joahannes@lrc.ic.unicamp.br>
2018-10-23 12:26:00 -0300

    Adicionado UDP/TCP

--- run_all.sh
+++ run_all.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+# Joahannes Costa <joahannes@lrc.ic.unicamp.br>
 
 #Estaticos
-./run_gerencia.sh 33 1 0 0 && ./run_gerencia.sh 33 2 0 0 && ./run_gerencia.sh 33 4 0 0 && ./run_gerencia.sh 33 6 0 0 && ./run_gerencia.sh 33 1 1 0 && ./run_gerencia.sh 33 2 1 0 && ./run_gerencia.sh 33 4 1 0 && ./run_gerencia.sh 33 6 1 0
+# (0) UDP
+./run_gerencia.sh 33 1 0 0 && ./run_gerencia.sh 33 2 0 0 && ./run_gerencia.sh 33 4 0 0 && ./run_gerencia.sh 33 6 0 0
+# (1) TCP
+./run_gerencia.sh 33 1 1 0 && ./run_gerencia.sh 33 2 1 0 && ./run_gerencia.sh 33 4 1 0 && ./run_gerencia.sh 33 6 1 0
+# (2) UDP/TCP
+./run_gerencia.sh 33 1 2 0 && ./run_gerencia.sh 33 2 2 0 && ./run_gerencia.sh 33 4 2 0 && ./run_gerencia.sh 33 6 2 0
 
 #Moveis
-./run_gerencia.sh 33 1 0 1 && ./run_gerencia.sh 33 2 0 1 && ./run_gerencia.sh 33 4 0 1 && ./run_gerencia.sh 33 6 0 1 && ./run_gerencia.sh 33 1 1 1 && ./run_gerencia.sh 33 2 1 1 && ./run_gerencia.sh 33 4 1 1 && ./run_gerencia.sh 33 6 1 1
+# (0) UDP
+./run_gerencia.sh 33 1 0 1 && ./run_gerencia.sh 33 2 0 1 && ./run_gerencia.sh 33 4 0 1 && ./run_gerencia.sh 33 6 0 1
+# (1) TCP
+./run_gerencia.sh 33 1 1 1 && ./run_gerencia.sh 33 2 1 1 && ./run_gerencia.sh 33 4 1 1 && ./run_gerencia.sh 33 6 1 1
+# (2) UDP/TCP
+./run_gerencia.sh 33 1 2 1 && ./run_gerencia.sh 33 2 2 1 && ./run_gerencia.sh 33 4 2 1 && ./run_gerencia.sh 33 6 2 1
 
 #email
 #python email_proc.py
