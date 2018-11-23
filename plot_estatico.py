588b075ceaaa70c978efd78dc06b9c13016578cf
Joahannes Costa <joahannes@lrc.ic.unicamp.br>
2018-11-23 17:20:34 -0200

    Update plot_estatico.py

--- plot_estatico.py
+++ plot_estatico.py
@@ -8,16 +8,19 @@ from matplotlib.backends.backend_pdf import PdfPages
 
 import numpy as np
 
-TRAFFICS=(1, 2, 4, 6)
-ALGORITHMS = ("UDP","TCP") 
-COLORS = {"UDP" : "k", "TCP" : "r"}
-MARKS = {"UDP" : "H", "TCP" : "^"}
-LINES = {"UDP" : "-", "TCP" : "--"}
+TRAFFICS=(1,2,4,6,)
+#TRAFFICS=(6,12,24,48,)
+ALGORITHMS = ("UDP", "TCP", "AMBOS") 
+COLORS = {"UDP" : "k", "TCP" : "r", "AMBOS" : "g"}
+MARKS = {"UDP" : "H", "TCP" : "^","AMBOS" : "s"}
+LINES = {"UDP" : "-", "TCP" : "--", "AMBOS" : "-."}
 
 mobilidade = "est" #Deixar automatico
 
-X_LEGEND = 'Número de Clientes'.decode('utf-8')
+X_LEGEND = 'Número de clientes'.decode('utf-8')
 formato = '.eps'
+
+LEGEND = ("CBR", "Rajada", "CBR/Rajada")
 
 # (0) Replication, (1) Host, (2) Clientes, (3) Vazao, (4) Delay, (5) Perdidos
 
@@ -32,10 +35,11 @@ def confidence_interval(values):
 #DELAY
 def plot_delay():
 
+	legend = 0
+
 	for algorithm in ALGORITHMS:
 		delay_values = []
 		confidence_intervals = []
-
 		delay = float(0)
 
 		for traffic in TRAFFICS:
@@ -46,55 +50,40 @@ def plot_delay():
 			delay = 0.0
 
 			for line in open(file_input_name):
-				if replication == int(line.split()[0]): 
-					if line.split()[4] != "0":
-						#print line.split()[3]
+				if replication == int(line.split()[0]):
+					if float(line.split()[4]) > 0.0000:
 						delay += float(line.split()[4])
-						#print delay
-						#values.append(delay)
 
 				else:
-				# CHANGED
-					if not delay == 0.0:
+					if not delay == 0.0000:
+						delay += float(line.split()[4])
 						values.append(delay)
 						delay = 0.0
-				#		print "aqui"
-
+						
 				while replication != int(line.split()[0]):
 					replication += 1
 			
-			if algorithm == "TCP":
-				values = np.divide(values,traffic*2)
-			else:
-				values = np.divide(values,traffic)
-			#values = np.multiply(values,60)
-			#print values
+
+			values = np.divide(values,traffic)
 
 			delay_values.append(np.mean(values))
-			#print(algorithm, " - mean:", np.mean(values))
 			confidence_intervals.append(confidence_interval(values))
-			#print(" - confidence: ", confidence_interval(values))
 		
-		plt.errorbar(TRAFFICS, delay_values, yerr=confidence_intervals, label=algorithm, color=COLORS[algorithm], marker=MARKS[algorithm], linestyle=LINES[algorithm], markersize = 8, linewidth = 2)
+		plt.errorbar(TRAFFICS, delay_values, yerr=confidence_intervals, label=LEGEND[legend], color=COLORS[algorithm], marker=MARKS[algorithm], linestyle=LINES[algorithm], markersize = 8, linewidth = 2)
+		legend += 1
 
-	#yticks = np.arange(50, 110, 10)
 	plt.xlabel(X_LEGEND)
-	plt.ylabel('Atraso (s)')
-	plt.xlim(0, 7)
-	#plt.ylim(0,1.5)
+	plt.ylabel('Atraso (ms)')
+	#plt.xlim(0, 7)
+	plt.xlim(4, 50) #descomentar para 6 a 48 nos
 	plt.xticks(TRAFFICS)
-	#plt.yticks(yticks)
-	#plt.xticks((60, 100, 150))
-	plt.title('Cenário sem Mobilidade'.decode('utf-8'))
-	
-	#plt.yscale("log")
 	
 	plt.grid()    
-	plt.legend(numpoints=1, loc=2)
+	plt.legend(numpoints=1, loc=1)
 	
 	fig = plt.gcf()
 	fig.set_size_inches(6.8, 5.5)
-	fig.savefig('delay_estatico'+formato, dpi=200)
+	fig.savefig('delay_estatico'+formato, dpi=200, bbox_inches = 'tight', pad_inches = 0.05)
 
 	print("OK!\n")
 	plt.show()
@@ -102,68 +91,54 @@ def plot_delay():
 #PERDA
 def plot_vazao():
 
+	legend = 0
+
 	for algorithm in ALGORITHMS:
+
 		vazao_values = []
 		confidence_intervals = []
-
 		vazao = float(0)
 
 		for traffic in TRAFFICS:
 			values = []
 			file_input_name = algorithm + "/summary-" + str(traffic) + "-" + mobilidade + ".txt"
-
 			replication = 0
 			vazao = 0.0
 
 			for line in open(file_input_name):
 				if replication == int(line.split()[0]): 
-					if line.split()[3] != "NA":
-						#print line.split()[3]
+					if float(line.split()[3]) > 0.001:
 						vazao += float(line.split()[3])
-						#print delay
-						#values.append(delay)
 
 				else:
 				# CHANGED
 					if not vazao == 0.0:
+						vazao += float(line.split()[3])
 						values.append(vazao)
 						vazao = 0.0
-				#		print "aqui"
 
 				while replication != int(line.split()[0]):
 					replication += 1
-				
-			if algorithm == "TCP":
-				values = np.divide(values,traffic*2)
-			else:
-				values = np.divide(values,traffic)
-			#print values
 
+			values = np.divide(values,traffic)
 			vazao_values.append(np.mean(values))
-			#print(algorithm, " - mean:", np.mean(values))
 			confidence_intervals.append(confidence_interval(values))
-			#print(" - confidence: ", confidence_interval(values))
 		
-		plt.errorbar(TRAFFICS, vazao_values, yerr=confidence_intervals, label=algorithm, color=COLORS[algorithm], marker=MARKS[algorithm], linestyle=LINES[algorithm], markersize = 8, linewidth = 2)
+		plt.errorbar(TRAFFICS, vazao_values, yerr=confidence_intervals, label=LEGEND[legend], color=COLORS[algorithm], marker=MARKS[algorithm], linestyle=LINES[algorithm], markersize = 8, linewidth = 2)
+		legend += 1
 
-	#yticks = np.arange(50, 110, 10)
 	plt.xlabel(X_LEGEND)
-	plt.ylabel('Vazao (Mbps)')
-	plt.xlim(0, 7)
-	#plt.ylim(0,1.5)
+	plt.ylabel('Vazão (Mbps)'.decode('utf-8'))
+	#plt.xlim(0, 7)
+	plt.xlim(4, 50) #descomentar para 6 a 48 nos
 	plt.xticks(TRAFFICS)
-	#plt.yticks(yticks)
-	#plt.xticks((60, 100, 150))
-	plt.title('Cenário sem Mobilidade'.decode('utf-8'))
-	
-	#plt.yscale("log")
-	
+
 	plt.grid()    
-	plt.legend(numpoints=1, loc=2)
+	plt.legend(numpoints=1, loc=1)
 	
 	fig = plt.gcf()
 	fig.set_size_inches(6.8, 5.5)
-	fig.savefig('vazao_estatico'+formato, dpi=200)
+	fig.savefig('vazao_estatico'+formato, dpi=200, bbox_inches = 'tight', pad_inches = 0.05)
 
 	print("OK!\n")
 	plt.show()
@@ -171,69 +146,59 @@ def plot_vazao():
 #PERDA
 def plot_perda():
 
+	legend = 0
+
 	for algorithm in ALGORITHMS:
+		
 		perda_values = []
 		confidence_intervals = []
-
-		perda = float(0)
+		perda = int(0)
 
 		for traffic in TRAFFICS:
 			values = []
 			file_input_name = algorithm + "/summary-" + str(traffic) + "-" + mobilidade + ".txt"
-			#print file_input_name
 			replication = 0
-			perda = 0.0
+			perda = 0
 
 			for line in open(file_input_name):
 				if replication == int(line.split()[0]): 
-					if line.split()[5] != "0":
-						#print line.split()[3]
-						perda += float(line.split()[5])
-						#print file_input_name, line, algorithm, replication, perda
-						#values.append(delay)
+					if int(line.split()[5]) > 0:
+						perda += ((int(line.split()[5]) * 100) / int(line.split()[6]))
+					else:
+						perda += 0.1
 
 				else:
 				# CHANGED
-					if not perda == 0.0:
+					if not perda == 0:
+						perda += ((int(line.split()[5]) * 100) / int(line.split()[6]))
 						values.append(perda)
-						perda = 0.0
-						#print "aqui"
+						perda = 0
 
 				while replication != int(line.split()[0]):
 					replication += 1
 			
-			#values = np.divide(values,traffic)
-			# if algorithm == "TCP":
-			# 	values = np.divide(values,traffic*2)
-			# else:
-			# 	values = np.divide(values,traffic)
-			#print values
-
+			values = np.divide(values,traffic)
 			perda_values.append(np.mean(values))
-			#print(algorithm, " - mean:", np.mean(values))
 			confidence_intervals.append(confidence_interval(values))
-			#print(" - confidence: ", confidence_interval(values))
 		
-		plt.errorbar(TRAFFICS, perda_values, yerr=confidence_intervals, label=algorithm, color=COLORS[algorithm], marker=MARKS[algorithm], linestyle=LINES[algorithm], markersize = 8, linewidth = 2)
+		plt.errorbar(TRAFFICS, perda_values, yerr=confidence_intervals, label=LEGEND[legend], color=COLORS[algorithm], marker=MARKS[algorithm], linestyle=LINES[algorithm], markersize = 8, linewidth = 2)
+		legend += 1
 
-	#yticks = np.arange(50, 110, 10)
+	yticks = np.arange(0, 70, 10)
 	plt.xlabel(X_LEGEND)
-	plt.ylabel('Perda de pacotes #')
+	plt.ylabel('Pacotes perdidos (%)'.decode('utf-8'))
 	plt.xlim(0, 7)
-	#plt.ylim(0,1.5)
+	#plt.xlim(4, 50) #descomentar para 6 a 48 nos
 	plt.xticks(TRAFFICS)
-	#plt.yticks(yticks)
-	#plt.xticks((60, 100, 150))
-	plt.title('Cenário sem Mobilidade'.decode('utf-8'))
-	
-	#plt.yscale("log")
-	
+	plt.yticks(yticks)
+	plt.ylim(-2.5,60)
+
 	plt.grid()    
-	plt.legend(numpoints=1, loc=2)
+	plt.legend(numpoints=1, ncol=1, loc=2)
 	
 	fig = plt.gcf()
 	fig.set_size_inches(6.8, 5.5)
-	fig.savefig('perda_estatico'+formato, dpi=200)
+	fig.savefig('perda_estatico'+formato, dpi=200, bbox_inches = 'tight', pad_inches = 0.05)
 
 	print("OK!\n")
 	plt.show()
